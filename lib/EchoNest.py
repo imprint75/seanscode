cb0e27fe876464d6619331d810780a458f447fa7
sean <imprint75@gmail.com>
2014-06-01 15:40:01 +0300

    updating logging; cleanup

--- lib/EchoNest.py
+++ lib/EchoNest.py
@@ -2,39 +2,36 @@ import logging
 import requests
 
 from django.conf import settings
-from django.utils import simplejson
 
-logger = logging.getLogger('apps')
+logger = logging.getLogger(__name__)
+
 
 def _make_call(options, *args, **kwargs):
-
     params = '&'.join(['api_key={}'.format(settings.EN_API_KEY),
                        'name={}'.format(options['value']),
                        'format=json'])
-
     r = requests.get('{}{}/{}?{}'.format(settings.ECHO_NEST_BASE_URL,
                                          options['type'],
                                          options['action'],
                                          params))
     res = r.json()
-
     if res['response']['status']['message'] == "Success":
         return res['response']
     else:
         return None
 
 
-def echo_lookup(name = None, type_ = "artist"):
-    results = _make_call({"type" : type_,
-                          "action" : "search",
-                          "value" : name})
-    return results[''.join([type_, 's'])]
+def echo_lookup(name=None, type="artist"):
+    results = _make_call({"type": type,
+                          "action": "search",
+                          "value": name})
+    return results[''.join([type, 's'])]
 
 
-def echo_artist_info(id, method = "images"):
-    results = _make_call({"type" : "artist",
-                          "action" : method,
-                          "value" : id})
+def echo_artist_info(id, method="images"):
+    results = _make_call({"type": "artist",
+                          "action": method,
+                          "value": id})
     return results[method]
 
 
